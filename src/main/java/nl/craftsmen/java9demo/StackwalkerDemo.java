e8aede864074d6cf81e7d4dcc4ffece2c9c40f7d
Michel Schudel <michel.schudel@gmail.com>
2017-11-24 16:02:35 +0100

    Initial commit

---
+++ src/main/java/nl/craftsmen/java9demo/StackwalkerDemo.java
@@ -0,0 +1,21 @@
+package nl.craftsmen.java9demo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StackwalkerDemo {
+
+    public static void main(String[] args) {
+        StackWalker.getInstance().forEach(System.out::println);
+        execute();
+    }
+
+    static void execute() {
+
+        List<StackWalker.StackFrame> collect = StackWalker
+                .getInstance()
+                .walk(stackFrameStream -> stackFrameStream.skip(0)
+                .collect(Collectors.toList()));
+        System.out.println(collect);
+    }
+}
